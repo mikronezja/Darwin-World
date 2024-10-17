d0c7d1a29458ca0c03579e8c3dd14c09953b0f9f
Szymon-Klodowski <klodowski.szymon.2004@gmail.com>
2024-10-17 14:27:15 +0200

    Oby zadzialalo

---
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -0,0 +1,22 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirections;
+
+import java.util.Arrays;
+
+public class OptionsParser {
+    public static MoveDirections[] translatingDirections(String[] args) {
+        MoveDirections[] directions = new MoveDirections[args.length] ;
+        int i = 0;
+        for (String arg : args) {
+            switch (arg) {
+                case "f" -> {directions[i] = MoveDirections.FORWARD; i++;}
+                case "b" -> {directions[i] = MoveDirections.BACKWARD; i++;}
+                case "r" -> {directions[i] = MoveDirections.RIGHT; i++;}
+                case "l" -> {directions[i] = MoveDirections.LEFT; i++;}
+            }
+
+        }
+        return Arrays.copyOfRange(directions, 0, i);
+    }
+}
