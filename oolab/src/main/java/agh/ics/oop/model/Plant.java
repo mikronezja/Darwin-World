686389fd3b430a01aeb28ae45bfe913a8e6f04e9
Szymon-Klodowski <klodowski.szymon.2004@gmail.com>
2025-01-11 20:54:29 +0100

    Add Globe Plant And positiongeneratorforplants

---
+++ oolab/src/main/java/agh/ics/oop/model/Plant.java
@@ -0,0 +1,21 @@
+package agh.ics.oop.model;
+
+public class Plant implements WorldElement {
+
+    private int energy;
+    private Vector2d position;
+
+    public Plant(int energy, Vector2d position) {
+        this.energy = energy;
+        this.position = position;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    @Override
+    public Vector2d getPosition() {
+        return position;
+    }
+}
