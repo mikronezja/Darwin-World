3210eee8ad6d877a8379d887243f1090e733a524
mikronezja <juliademi05@gmail.com>
2025-01-20 14:50:53 +0100

    some tests added + move corrected

--- oolab/src/test/java/agh/ics/oop/model/GlobeTest.java
+++ oolab/src/test/java/agh/ics/oop/model/GlobeTest.java
@@ -2,19 +2,41 @@ package agh.ics.oop.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GlobeTest {
 
+    Globe globe = new Globe(10,10, 10, 10, 10, false);
+
     @Test
-    void place()
-    {
+    void checkIfGlobePlacesExactlyAsManyAnimalsAsIWant() {
+        for (int i = 0; i < 40; i++)
+        {
+            try
+            {
+            globe.place( new Animal( new Vector2d((int)Math.random()*10,(int)Math.random()*10),3, 2, 2,2, 2,2, false ));
+            } catch (IncorrectPositionException e)
+            {
+                e.printStackTrace();
+                fail("IncorrectPositionException");
+            }
+        }
 
-
+        int counter = 0;
+        for (Animal animal : globe.getAnimalsList())
+        {
+            counter++;
+        }
+        assertEquals(40, counter);
     }
 
     @Test
-    void move() {
+    void moveRemovesExactlyThisAnimalFromPosition()
+    {
+
     }
 
     @Test
@@ -23,6 +45,12 @@ class GlobeTest {
 
     @Test
     void killAnimal() {
+    }
+
+    @Test
+    void reproduce()
+    {
+
     }
 
     @Test
