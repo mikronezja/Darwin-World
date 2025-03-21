40f2fd0c364cebf93b3029944580d971d1ed875c
mikronezja <juliademi05@gmail.com>
2025-03-21 19:52:48 +0100

    commit

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -26,8 +26,6 @@ public class World {
         System.out.println(position2);
         System.out.println(position1.add(position2));
 
-        Animal krowa = new Animal();
-        System.out.println(krowa.toString());
 
         List<MoveDirections> directionsToSimulation=List.of();
         try {
